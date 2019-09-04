4d0f280e6b80e67385f6ab23e5000413fdaadd38
Tenzin Sean <tenzin.jeunesse@gmail.com>
2019-09-04 23:25:54 +0200

    added data layer

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -1,11 +1,15 @@
 package com.vega.springit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class SpringitApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
     }
