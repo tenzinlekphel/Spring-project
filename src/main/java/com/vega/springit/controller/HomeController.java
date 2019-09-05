55663016c773c050a4d3c09d97bdd380f7bcc3e0
Tenzin Sean <tenzin.jeunesse@gmail.com>
2019-09-05 17:00:58 +0200

    controller

---
+++ src/main/java/com/vega/springit/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.vega.springit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model, HttpServletRequest request) {
+        model.addAttribute( "Message", "Helloworld");
+        return "index";
+    }
+}
---
+++ src/main/java/com/vega/springit/controller/LinkController.java
@@ -0,0 +1,46 @@
+package com.vega.springit.controller;
+
+import com.vega.springit.domain.Link;
+import com.vega.springit.repository.LinkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/links/")
+public class LinkController {
+
+    private LinkRepository linkRepository;
+
+    public LinkController(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    @GetMapping("/")
+    public List<Link> list() {
+        return linkRepository.findAll();
+    }
+
+    @PostMapping("/create")
+    public Link create(@ModelAttribute Link link) {
+        return linkRepository.save(link);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Link> read(@PathVariable Long id) {
+        return linkRepository.findById(id);
+    }
+
+
+    @PutMapping("/update")
+    public Link update(@ModelAttribute Link link) {
+        return linkRepository.save(link);
+    }
+
+    @PutMapping("/update")
+    public void delete(@PathVariable Long id) {
+        linkRepository.deleteById((id);
+    }
+}
