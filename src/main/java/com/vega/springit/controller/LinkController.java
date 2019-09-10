4e12f277b1eab0435a1f905d82ba74a090265e4d
Tenzin Sean <tenzin.jeunesse@gmail.com>
2019-09-10 21:25:08 +0200

    update controller part

--- src/main/java/com/vega/springit/controller/LinkController.java
+++ src/main/java/com/vega/springit/controller/LinkController.java
@@ -1,9 +1,20 @@
 package com.vega.springit.controller;
 
+import com.vega.springit.domain.Comment;
 import com.vega.springit.repository.LinkRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import com.vega.springit.domain.Link;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.vega.springit.service.LinkService;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
 
 @Controller
 public class LinkController {
@@ -15,8 +26,42 @@ public class LinkController {
 
     @GetMapping("/")
     public String list(Model model) {
-        model.addAttribute("links", linkRepository.findAll());
+        model.addAttribute("link", linkRepository.findAll());
         return "link/list";
+    }
+
+    @GetMapping("/link/{id}")
+    public String read(@PathVariable Long id, Model model) {
+        Optional<Link> link = linkRepository.findById(id);
+        if ( link.isPresent() ) {
+            model.addAttribute("link", link.get());
+            return "link/view";
+        } else{
+            return "redirect:/";
+        }
+    }
+
+    @GetMapping("/link/submit")
+    public String newLinkForm(Model model) {
+        model.addAttribute("link", new Link());
+        return "link/submit";
+    }
+
+    @PostMapping("/link/submit")
+    public String createLink(@Valid Link link, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if( bindingResult.hasErrors() ) {
+            logger.info("Validation errors were found while submitting a new link.");
+            model.addAttribute("link",link);
+            return "link/submit";
+        } else {
+            // save our link
+            linkService.save(link);
+            logger.info("New link was saved successfully");
+            redirectAttributes
+                    .addAttribute("id",link.getId())
+                    .addFlashAttribute("success",true);
+            return "redirect:/link/{id}";
+        }
     }
 
 }
